e456b4a9a093bf7a30d6998ced23234f626cc691
zhangyanan <2567051886@qq.com>
2021-04-17 21:23:25 +0800

    first project

---
+++ src/main/java/com/ccorg/springcloudccorg/SpringcloudCcorgApplication.java
@@ -0,0 +1,13 @@
+package com.ccorg.springcloudccorg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudCcorgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudCcorgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ccorg/springcloudccorg/SpringcloudCcorgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccorg.springcloudccorg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudCcorgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
